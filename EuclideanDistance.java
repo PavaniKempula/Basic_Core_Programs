48c4efd9cef74e62dc0f3808860649755bf0f290
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-31 05:17:36 +0530

    ADDED : It takes 2 integer from user to calculate distance using euclidean distance formula

---
+++ EuclideanDistance.java
@@ -0,0 +1,38 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @author Pavani
+ *Program that finds distance using Euclidean distance formula
+ */
+public class EuclideanDistance {
+
+	/*
+	 * Name : findEuclideandistance
+	 * Description : Finding distance
+	 * Algorithm : using Euclidean distance formula calculating distance
+	 * float distance=(float) Math.sqrt((x*x)+(y*y));
+	 * 
+	 * @param x  //first input value from user
+	 * @param y  //second input value from user
+	 */
+	public static void findEuclideandistance(int x, int y) {
+		float distance = (float) Math.sqrt((x * x) + (y * y));
+		System.out.println("Euclidean Distance of (" + x + "," + y + ") is " + distance);
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Calculating Euclidean Distance");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("enter the value of x point:");
+		int x = scanner.nextInt();
+		System.out.println("enter the value of y point:");
+		int y = scanner.nextInt();
+		scanner.close();
+
+		findEuclideandistance(x, y);
+	}
+
+}
