1f61d358f784abe6660828c2cec86064367bf79d
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-31 04:10:28 +0530

    ADDED : To find given Alphabet is Vowel or Consonant

---
+++ VowelOrConsonant.java
@@ -0,0 +1,32 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @author Pavani
+ * VowelOrConsonant java class finding given alphabet is vowel or consonant
+ * Vowel are predefined in program
+ * asking user to enter alphabet if that alphabet matches with vowel then it is vowel or it is consonant
+ */
+public class VowelOrConsonant {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Alphabet");
+		String userInput = scanner.next();
+		scanner.close();
+		switch (userInput) {
+		case "a":
+		case "e":
+		case "i":
+		case "o":
+		case "u":
+			System.out.println(userInput + " is vowel.");
+			break;
+		default:
+			System.out.println(userInput + " is consonant.");
+		}
+	}
+
+}
