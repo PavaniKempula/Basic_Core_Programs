3c11c167cb9e98a855b9dc5c7d0b6d6e0d29688c
PavaniKempula <pavanikempula1998@gmail.com>
2021-07-31 04:02:58 +0530

    ADDED : To check given number is even or odd

---
+++ EvenOrOdd.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @author Pavani 
+ * EvenOrOdd java file accept number from user 
+ * checks if that number is even or odd
+ */
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number to check if it is even or odd : ");
+		int userNumber = scanner.nextInt();
+		scanner.close();
+
+		if (userNumber % 2 == 0) {
+			System.out.println("The given number : " + userNumber + " " + "is a even number.");
+		} else {
+			System.out.println("The given number : " + userNumber + " " + "is a odd number.");
+		}
+	}
+
+}
